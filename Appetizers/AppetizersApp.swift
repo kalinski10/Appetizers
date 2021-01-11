78095f070e2bca633c8ddc84f3133bc4c75dc18a
kalin balabnov <kalin_dude10@live.com>
2021-01-11 16:07:21 +0000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Kalin Balabanov on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
